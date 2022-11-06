6cf6356db4c4a0899a43a8e6d05fd1bf5970d402
yamano <ton_1117@hotmail.com>
2022-11-06 12:38:49 +0700

    add search data

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,14 @@ import HomeView from "./views/HomeView.vue";
     <div class="text-2xl text-white">Pokedex</div>
   </div>
   <div
-    class="p-4 lg:p-8 bg-base-300 rounded-b-box rounded-tr-box relative overflow-x-auto"
+    class="body backdrop:p-4 lg:p-8 bg-base-300 rounded-b-box rounded-tr-box relative overflow-x-auto"
   >
     <RouterView></RouterView>
     <!-- <HomeView></HomeView> -->
   </div>
 </template>
+<style>
+.body {
+  min-height: 100vh;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import PokemonCard from "@/components/PokemonCard.vue";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { getPokemons } from "@/services/pokemon.services";
 import type { Pokemon } from "@/models/pokemon.model";
 import { useRouter } from "vue-router";
@@ -8,6 +8,7 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const pokemonList = ref<Pokemon[]>(); //ประกาศ type พร้อมนำ Interface ที่เป็น type มาใช้
+const seachValue = ref();
 
 onMounted(async () => {
   const result = await getPokemons();
@@ -17,47 +18,54 @@ onMounted(async () => {
 // const onNavigateDetail = (name: string) => alert(name);
 const onNavigateDetail = (name: string) =>
   router.push({ name: "detail", params: { name: name } });
+
+const filterPokemon = computed(() => {
+  if (!pokemonList.value) {
+    return pokemonList.value;
+  }
+  return pokemonList.value?.filter((pokemon) =>
+    pokemon.name.includes(seachValue.value)
+  );
+});
 </script>
 <template>
-  <div>
-    <div>
-      <div class="form-control justify-center">
-        <div class="input-group">
-          <input
-            type="text"
-            placeholder="Search…"
-            class="input input-bordered"
-          />
-          <button class="btn btn-square">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="cardContainer mw-[1024px] mx-auto">
-      <PokemonCard
-        class="pokemonCard"
-        v-for="(pokemon, index) of pokemonList"
-        :key="index"
-        :img="pokemon.img"
-        :name="pokemon.name"
-        :buttonText="''"
-        @clickbtn="onNavigateDetail($event)"
+  <div class="flex justify-center form-control">
+    <div class="input-group">
+      <input
+        v-model="seachValue"
+        type="text"
+        placeholder="Search…"
+        class="input input-bordered"
       />
+      <button class="btn btn-square">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
     </div>
+  </div>
+
+  <div class="cardContainer mw-[1024px] mx-auto">
+    <PokemonCard
+      class="pokemonCard"
+      v-for="(pokemon, index) of filterPokemon"
+      :key="index"
+      :img="pokemon.img"
+      :name="pokemon.name"
+      :buttonText="''"
+      @clickbtn="onNavigateDetail($event)"
+    />
   </div>
 </template>
 <style scoped>
